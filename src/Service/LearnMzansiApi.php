5571eb643159c9145b42da32b6d4dcbd9fb16062
benedictnkosi <nkosi.benedict@gmail.com>
2025-02-03 18:17:14 +0200

    test

--- src/Service/LearnMzansiApi.php
+++ src/Service/LearnMzansiApi.php
@@ -317,7 +317,7 @@ class LearnMzansiApi extends AbstractController
             }
 
             if ($learner->getName() == 'admin') {
-                $statusCondition = ' AND q.status = \'new\' ';
+                $statusCondition = ' AND q.status = \'approved\' ';
             } else {
                 $statusCondition = ' AND q.status = \'approved\' ';
             }
