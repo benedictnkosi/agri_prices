e82ad948cd4981c64a4ae63f1cb122a41cf00e18
benedictnkosi <nkosi.benedict@gmail.com>
2025-02-03 18:26:40 +0200

    fix: correct indentation in validation for multiple_choice and multi_select options

--- src/Service/LearnMzansiApi.php
+++ src/Service/LearnMzansiApi.php
@@ -157,11 +157,13 @@ class LearnMzansiApi extends AbstractController
             // Validate that options are not empty for multiple_choice or multi_select types
             if (($data['type'] == 'multiple_choice' || $data['type'] == 'multi_select')) {
                 if (empty($data['options']['option1']) || empty($data['options']['option2']) || empty($data['options']['option3']) || empty($data['options']['option4'])) {
-                return array(
-                    'status' => 'NOK',
-                    'message' => "Options cannot be empty for multiple_choice or multi_select types."
-                );
+                    return array(
+                        'status' => 'NOK',
+                        'message' => "Options cannot be empty for multiple_choice or multi_select types."
+                    );
+                }
             }
+
 
             //check that the expected answer is not too long
             //spit answer by |
